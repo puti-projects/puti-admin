c3609aa90e594702c9428b262987cc484c10e6f2
gzp <gzp@goozp.com>
2018-05-12 02:40:44 +0800

    init to element UI

--- src/app.vue
+++ src/app.vue
@@ -1,26 +1,30 @@
 <template>
-    <div id="main" class="app-main">
-        <router-view></router-view>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <el-button @click="startHacking">Start</el-button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted () {
-
-        },
-        beforeDestroy () {
-
-        },
-        methods: {
-
-        }
-    };
+export default {
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
+}
 </script>
 
 <style>
-.app-main{
-    width: 100%;
-    height: 100%;
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
 }
 </style>
